68f09f0cd496b0acc2bf4b3609b7fa5173b98c12
Rui Tobias Carvalho <bibolook@gmail.com>
2026-07-15 10:32:21 -0300

    Fix app base path and remove default favicon from index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/simulador-de-p-ndulo-e-mhs/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
