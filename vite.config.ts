22f310a31949f86253b5a4abbb0302f754975df0
Rui Tobias Carvalho <bibolook@gmail.com>
2026-07-15 10:14:25 -0300

    set Vite base for GitHub Pages and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/simulador-de-p-ndulo-e-mhs/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
